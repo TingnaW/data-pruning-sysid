978afee1c7c5d40e94df2f165f48710221e308f0
sikai zhang <matthew.szhang91@gmail.com>
2025-08-19 16:04:43 +0800

    linter

--- plot_phase_portraits.py
+++ plot_phase_portraits.py
@@ -20,18 +20,28 @@ def main():
     dot_y_min = np.nanmin(sol[:, :, 1])
     dot_y_max = np.nanmax(sol[:, :, 1])
     y, dot_y = np.meshgrid(
-        np.linspace(y_min, y_max, 30),
-        np.linspace(dot_y_min, dot_y_max, 30)
+        np.linspace(y_min, y_max, 30), np.linspace(dot_y_min, dot_y_max, 30)
     )
     sol = autonomous_dual_stable_equilibria((y, dot_y))
     ddot_y = sol[1]
-    plt.streamplot(y, dot_y, dot_y, ddot_y, color="tab:blue", density=1.2, minlength=0.02, maxlength=0.1, linewidth=0.5, arrowsize=0.5)
+    plt.streamplot(
+        y,
+        dot_y,
+        dot_y,
+        ddot_y,
+        color="tab:blue",
+        density=1.2,
+        minlength=0.02,
+        maxlength=0.1,
+        linewidth=0.5,
+        arrowsize=0.5,
+    )
     fonts = 13
     plt.xlabel("y(t)", fontsize=fonts)
     plt.ylabel("dy/dt(t)", fontsize=fonts)
     offset = 0.02
-    plt.xlim(y_min-offset, y_max+offset)
-    plt.ylim(dot_y_min-offset, dot_y_max+offset)
+    plt.xlim(y_min - offset, y_max + offset)
+    plt.ylim(dot_y_min - offset, dot_y_max + offset)
     plt.tick_params(axis="both", labelsize=fonts)
     plt.savefig(figure_name, bbox_inches="tight")
     print("Image " + figure_name + " has been generated.")
